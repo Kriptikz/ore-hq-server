9b85c92943662bb4a1bd8ab0024634ebd702b8c3
Kriptikz <Kriptikz@users.noreply.github.com>
2024-10-28 15:37:43 -0500

    increase mining cutoff by 1 second to get closer to 55 second mining goal

--- src/systems/handle_ready_clients_system.rs
+++ src/systems/handle_ready_clients_system.rs
@@ -45,7 +45,7 @@ pub async fn handle_ready_clients_system(
                 let latest_proof = lock.clone();
                 drop(lock);
 
-                let cutoff = get_cutoff(latest_proof, 5);
+                let cutoff = get_cutoff(latest_proof, 4);
                 let mut should_mine = true;
 
                 // only distribute challenge if 10 seconds or more is left
