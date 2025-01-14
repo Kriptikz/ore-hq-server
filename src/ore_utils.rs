77cf633bb6f6444eb8569e6bbae2a5868221ca9f
Kriptikz <Kriptikz@users.noreply.github.com>
2025-01-14 10:58:03 -0600

    add MineEventWithGlobalBoosts to ore_utils

--- src/ore_utils.rs
+++ src/ore_utils.rs
@@ -32,6 +32,21 @@ pub struct MineEventWithBoosts {
 
 event!(MineEventWithBoosts);
 
+#[repr(C)]
+#[derive(Clone, Copy, Debug, PartialEq, Pod, Zeroable)]
+pub struct MineEventWithGlobalBoosts {
+    pub balance: u64,
+    pub difficulty: u64,
+    pub last_hash_at: i64,
+    pub timing: i64,
+    pub net_reward: u64,
+    pub net_base_reward: u64,
+    pub net_miner_boost_reward: u64,
+    pub net_staker_boost_reward: u64,
+}
+
+event!(MineEventWithGlobalBoosts);
+
 pub fn get_auth_ix(signer: Pubkey) -> Instruction {
     let proof = get_proof_pda(signer);
 
